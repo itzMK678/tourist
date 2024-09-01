317a0ac31758ef6b3321559f3eb821a3a392766a
ItzMK <raomamoon592@gmail.com>
2024-09-01 11:38:29 +0500

    first commit

---
+++ logic.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu-box");
+const menubtn = document.getElementById("menu-btn");
+
+function menushow() {
+  if (menu.style.display == "none") {
+    menu.style.display = "block";
+  } else{
+    menu.style.display = "none";
+  }
+}
+
+
